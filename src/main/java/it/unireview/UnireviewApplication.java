ac9c275c2a4a0684c806f804373b0526f6f047c2
danlig <dliguori03@gmail.com>
2024-02-22 17:52:17 +0100

    first commit

---
+++ src/main/java/it/unireview/UnireviewApplication.java
@@ -0,0 +1,13 @@
+package it.unireview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnireviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnireviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/unireview/UnireviewApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unireview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnireviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
